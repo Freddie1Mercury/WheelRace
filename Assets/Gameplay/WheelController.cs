c96c83d6bf8b7492e9931b2d25f34253743ebbbc
Freddie1Mercury <kirillostrovskylol@mail.ru>
2024-09-11 01:26:45 +0300

    update rigedbody configirates

--- Assets/Gameplay/WheelController.cs
+++ Assets/Gameplay/WheelController.cs
@@ -6,6 +6,6 @@ public class WheelController : MonoBehaviour
 {
    public void StartForce()
     {
-        transform.GetComponent<Rigidbody>().AddForce(new Vector3 (-30, 0, 0), ForceMode.Impulse);
+        transform.GetComponent<Rigidbody>().AddForce(new Vector3 (-40, 0, 0), ForceMode.Impulse);
     }
 }
