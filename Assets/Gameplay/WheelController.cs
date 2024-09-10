bbef93f29a8457414009e8d05ed18c7faf66b635
Freddie1Mercury <kirillostrovskylol@mail.ru>
2024-09-10 21:31:17 +0300

    fix camera under texture

--- Assets/Gameplay/WheelController.cs
+++ Assets/Gameplay/WheelController.cs
@@ -6,6 +6,6 @@ public class WheelController : MonoBehaviour
 {
    public void StartForce()
     {
-        transform.GetComponent<Rigidbody>().AddForce(new Vector3 (50, 0, 0), ForceMode.Impulse);
+        transform.GetComponent<Rigidbody>().AddForce(new Vector3 (-30, 0, 0), ForceMode.Impulse);
     }
 }
