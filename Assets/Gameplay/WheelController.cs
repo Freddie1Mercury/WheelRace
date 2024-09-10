5f2bf008b1a97c44d096042292beeafb25c7ac0d
Freddie1Mercury <kirillostrovskylol@mail.ru>
2024-09-10 19:12:12 +0300

    camera controll and start force

---
+++ Assets/Gameplay/WheelController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+   public void StartForce()
+    {
+        transform.GetComponent<Rigidbody>().AddForce(new Vector3 (50, 0, 0), ForceMode.Impulse);
+    }
+}
