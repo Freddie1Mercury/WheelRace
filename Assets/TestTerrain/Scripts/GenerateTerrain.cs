5b4c3934795d855182b9295a1d94711b9aaf130f
Vladimir-Danilin <ivve.2016@yandex.ru>
2024-09-11 03:34:55 +0600

    design commit

---
+++ Assets/TestTerrain/Scripts/GenerateTerrain.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour
+{
+    //[SerializeField]
+    //private List<Terrain> terrainPrefabs; // Список префабов террейна
+    //[SerializeField]
+    //private int gridSize = 5; // Размер сетки
+    //[SerializeField]
+    //private float terrainSize = 100f; // Размер одного террейна (ширина и длина)
+
+    //void Start()
+    //{
+    //    Generate();
+    //}
+
+    //void Generate()
+    //{
+    //    for (int x = 0; x < gridSize; x++)
+    //    {
+    //        for (int z = 0; z < gridSize; z++)
+    //        {
+    //            Terrain terrainPrefab = terrainPrefabs[Random.Range(0, terrainPrefabs.Count)];
+    //            Vector3 spawnPosition = new Vector3(x * terrainSize, 0, z * terrainSize);
+    //            Instantiate(terrainPrefab, spawnPosition, Quaternion.identity); 
+    //        }
+    //    }
+    //}
+}
