4633299089e28200608669e12ffcc745ef708b5e
Freddie1Mercury <kirillostrovskylol@mail.ru>
2024-09-17 14:33:07 +0300

    Money System and Upgrades version 0.5

--- Assets/Gameplay/EndGame.cs
+++ Assets/Gameplay/EndGame.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EndGame : MonoBehaviour
@@ -7,8 +8,12 @@ public class EndGame : MonoBehaviour
     public Vector3 StartPosition { get { return _startPosition; } }
 
     [SerializeField] private WheelController _wheelController;
+    [SerializeField] private MoneyManager _moneyManager;
+
     [SerializeField] private GameObject _deathPanel;
     [SerializeField] private GameObject _wheel;
+
+    [SerializeField] private List<GameObject> _uslessInGameSessionUi = new List<GameObject>();
 
     private void Awake()
     {
@@ -16,17 +21,24 @@ public class EndGame : MonoBehaviour
         _startPosition = _wheel.transform.position;
     }
 
-    private void Update()
+    public void Death()
     {
-        if (_wheelController.WheelIsLive == false)
+        if (_wheel.transform.position != _startPosition)
         {
-            if (_wheel.transform.position != _startPosition)
+            _deathPanel.SetActive(true);
+            _wheel.transform.GetComponent<Rigidbody>().isKinematic = true;
+            _wheel.transform.position = _startPosition;
+            _wheel.transform.rotation = _startRotation;
+            // отключения ui который используется в матче
+            foreach (var UiElement in _uslessInGameSessionUi)
             {
-                _deathPanel.SetActive(true);
-                _wheel.transform.GetComponent<Rigidbody>().isKinematic = true;
-                _wheel.transform.position = _startPosition;
-                _wheel.transform.rotation = _startRotation;
+                UiElement.SetActive(false);
             }
+            _moneyManager.AddMoneyForLastSession();
+            Debug.Log("Гений алгоритма");
         }
+
     }
+
+    
 }
--- Assets/Gameplay/WheelController.cs
+++ Assets/Gameplay/WheelController.cs
@@ -1,53 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WheelController : MonoBehaviour
 {
+    private int _dashForwardForce;
+    private float _cooldownDashForward;
+    private float _remainingTimeUntilDashForward;
     private bool _wheelIslive = false;
+
+    public float CooldownDashForward { get => _cooldownDashForward; set => _cooldownDashForward = value < 0 ? 0 : value; }
+    public int DashForwardForce { get => _dashForwardForce; set => _dashForwardForce = value < 0 ? 0 : value; }
     public bool WheelIsLive => _wheelIslive;
+
     [SerializeField] private EndGame _endGame;
+    [SerializeField] private Rigidbody _rigedbodyWheel;
+    [SerializeField] private int _startForce;
+    [SerializeField] private GameObject _dashForwardButton;
+    [SerializeField] private Upgrades _upgrades;
+    [SerializeField] private Image _dashForwardImage;
 
     private void Start()
     {
-      
+        _cooldownDashForward = 20;
+        _remainingTimeUntilDashForward = _cooldownDashForward;
         StartCoroutine(CheckDeath());
-        
+        _startForce = 30;
     }
+
+    private void Update()
+    {
+        if (_remainingTimeUntilDashForward <= _cooldownDashForward)
+        {
+            _dashForwardImage.fillAmount = _remainingTimeUntilDashForward / _cooldownDashForward;
+            _remainingTimeUntilDashForward += Time.deltaTime;
+        }
+        else if (_dashForwardImage.fillAmount >= 0.9)
+        {
+            _dashForwardImage.fillAmount = 1;
+        }
+    }
+
     private IEnumerator CheckDeath()
     {
         while (true)
         {
-            if (transform.GetComponent<Rigidbody>().velocity.x >= -0.2 && transform.position != _endGame.StartPosition)
+            if (_rigedbodyWheel.velocity.x >= -0.2 && transform.position != _endGame.StartPosition)
             {
                 yield return new WaitForSeconds(2);
-                if (transform.GetComponent<Rigidbody>().velocity.x >= -0.2 && transform.position != _endGame.StartPosition)
+                if (_rigedbodyWheel.velocity.x >= -0.2 && transform.position != _endGame.StartPosition)
                 {
                     _wheelIslive = false;
-                    yield return new WaitForSeconds(1);
-
+                    // костыль
+                    yield return new WaitForSeconds(0.1f);
+                    // костыль закончился
+                    _endGame.Death();
                 }
             }
             else
             {
-            yield return null;
+                yield return null;
             }
         }
     }
+
     public void StartForce()
     {
+        if (_upgrades.DashForwardLevel > 0) { _dashForwardButton.SetActive(true); }
+
         _wheelIslive = true;
-        transform.GetComponent<Rigidbody>().isKinematic = false;
-        transform.GetComponent<Rigidbody>().AddForce(new Vector3(-30, 0, 0), ForceMode.Impulse);
+        _rigedbodyWheel.isKinematic = false;
+        _rigedbodyWheel.AddForce(new Vector3(-_startForce, 0, 0), ForceMode.Impulse);
     }
 
     public void DashLeft()
     {
-        transform.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -20), ForceMode.Impulse);
+        _rigedbodyWheel.AddForce(new Vector3(0, 0, -20), ForceMode.Impulse);
     }
 
     public void DashRight()
     {
-        transform.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 20), ForceMode.Impulse);
+        _rigedbodyWheel.AddForce(new Vector3(0, 0, 20), ForceMode.Impulse);
+    }
+
+    public void DashForward()
+    {
+
+        if (_dashForwardImage.fillAmount == 1)
+        {
+            _remainingTimeUntilDashForward = 0;
+            _rigedbodyWheel.AddForce(new Vector3(-_dashForwardForce, 0, 0), ForceMode.Impulse);
+        }
     }
 }
